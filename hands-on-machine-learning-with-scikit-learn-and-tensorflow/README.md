5d4304d4e6983f154cf643d0acc1734cd6b59665
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-07-23 10:45:11 -0700

    Add README and chap2 load_data.

---
+++ hands-on-machine-learning-with-scikit-learn-and-tensorflow/README.md
@@ -0,0 +1,7 @@
+# Hands-On Machine Learning with Scikit-Learn and TensorFlow
+
+My exercises for the book <Hands-On Machine Learning with Scikit-Learn and TensorFlow>.
+
+On Amazon: https://www.amazon.com/Hands-Machine-Learning-Scikit-Learn-TensorFlow/dp/1491962291
+
+On Douban: https://book.douban.com/subject/26840215/
---
+++ hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap02_HousingPrice/load_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pyhon
+"""Load data."""
+
+import os
+import pandas as pd
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), '../data')
+
+
+def load_housing_data():
+    """Load housing.csv data."""
+    csv_path = os.path.join(DATA_DIR, "handson-ml/datasets/housing/housing.csv")
+    return pd.read_csv(csv_path)
