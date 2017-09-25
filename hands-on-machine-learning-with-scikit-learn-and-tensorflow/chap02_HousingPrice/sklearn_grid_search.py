4db79071f47b97b195d56c7f0e3b7f8aa9a0599e
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-09-25 23:17:28 +0800

    Add sklearn_grid_search.

---
+++ hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap02_HousingPrice/sklearn_grid_search.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Scikit learn random forest."""
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+from sklearn_pipeline import CombinedAttributesAdder
+import numpy as np
+
+import load_data
+import sklearn_feature_union
+
+
+def main():
+    """Main function."""
+    training_set, test_set = load_data.split_train_and_test_set()
+    housing, label = load_data.split_dataframe_column(training_set,
+                                                      'median_house_value')
+    CombinedAttributesAdder.compute_index(housing)
+
+    housing_training = sklearn_feature_union.prepare_data(housing)
+
+    # Train
+    param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        {'bootstrap': [False], 'n_estimators': [3, 10],
+         'max_features': [2, 3, 4]},
+    ]
+
+    forest_reg = RandomForestRegressor()
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                               scoring='neg_mean_squared_error')
+    grid_search.fit(housing_training, label)
+
+    # Evaluate
+    housing, label = load_data.split_dataframe_column(test_set,
+                                                      'median_house_value')
+    housing_test = sklearn_feature_union.prepare_data(housing)
+    predition = grid_search.predict(housing_test)
+    grid_mse = mean_squared_error(label, predition)
+    grid_rmse = np.sqrt(grid_mse)
+    print('Best params = {}'.format(grid_search.best_params_))
+    print('root_mean_squared_error = {}'.format(grid_rmse))
+
+if __name__ == '__main__':
+    main()
