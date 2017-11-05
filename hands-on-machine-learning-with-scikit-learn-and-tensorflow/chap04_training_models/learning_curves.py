509371095cc34bf808fff1c8f598c12ea5d3b1e3
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-11-05 12:58:02 -0800

    Add polynomial regression learning curves.

--- hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap04_training_models/learning_curves.py
+++ hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap04_training_models/learning_curves.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def plot_learning_curves(model, X, y):
@@ -19,11 +21,23 @@ def plot_learning_curves(model, X, y):
         val_errors.append(mean_squared_error(y_val_predict, y_val))
     plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
     plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
-    plt.show()
 
 if __name__ == '__main__':
     m = 100
     X = 6 * np.random.rand(m, 1) - 3
     y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
+
+    # Linear regression learning curves.
     lin_reg = LinearRegression()
+    plt.subplot(121)
     plot_learning_curves(lin_reg, X, y)
+
+    # Polynomial regression learning curves.
+    poly_reg = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=2, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+    plt.subplot(122)
+    plot_learning_curves(poly_reg, X, y)
+
+    plt.show()
--- hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap04_training_models/polynomial_regression.py
+++ hands-on-machine-learning-with-scikit-learn-and-tensorflow/chap04_training_models/polynomial_regression.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
 if __name__ == '__main__':
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     X = 6 * np.random.rand(m, 1) - 3
     y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
 
+    # Step by step.
     poly_features = PolynomialFeatures(degree=2, include_bias=False)
     X_poly = poly_features.fit_transform(X)
 
@@ -20,6 +22,22 @@ if __name__ == '__main__':
     X_sorted = X_poly[X_poly[:, 0].argsort()]
     y_predict = lin_reg.predict(X_sorted)
 
+    plt.subplot(121)
     plt.plot(X, y, "b.")
     plt.plot(X_sorted[:,0], y_predict, "r-")
+
+    # Pipeline.
+    poly_reg = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=2, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+    poly_reg.fit(X, y)
+
+    X_sorted = X[X[:, 0].argsort()]
+    y_predict = poly_reg.predict(X_sorted)
+
+    plt.subplot(122)
+    plt.plot(X, y, "b.")
+    plt.plot(X_sorted, y_predict, "r-")
+
     plt.show()
