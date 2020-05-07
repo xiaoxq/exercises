cc90bb47140012006d63f8bc8e8a4d16de7a93f2
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2020-05-07 08:08:10 -0700

    Add hello-world of <the-rust-programming-language>

---
+++ 2020_the-rust-programming-language/1_getting-started/0_hello-world.rs
@@ -0,0 +1,5 @@
+// Compile: rustc 0_hello-world.rs -o main
+// Run:     ./main
+fn main() {
+    println!("Hello, world!");
+}
---
+++ 2020_the-rust-programming-language/README.md
@@ -0,0 +1,7 @@
+# The Rust Programming Language (Covers Rust 2018)
+
+## Setup
+
+```bash
+sudo apt install rustc
+```
